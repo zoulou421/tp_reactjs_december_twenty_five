07b4304bfc0c0e3d7113a28c00f5e6951c34ea52
zoulou421 <bonevybeby@gmail.com>
2025-12-16 14:15:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const API_URL = 'https://www.omdbapi.com?apikey=d2b20d60';
+import MovieCard from "./MovieCard";import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=d2b20d60";
 
 const App = () => {
-    const searchMovies = async (title = "Batman") => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data);
-    };
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        searchMovies(); // Appelle la recherche au chargement
+        searchMovies("Batman");
     }, []);
 
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
     return (
-        <h1>Hello APP</h1>
+        <div className="app">
+            <h1>AFINDA MOVIE</h1>
+
+            <div className="search">
+                <input
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search for movies"
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )}
+        </div>
     );
 };
 
---
+++ src/AppOld.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+const API_URL = 'https://www.omdbapi.com?apikey=d2b20d60';
+
+const App = () => {
+    const searchMovies = async (title = "Batman") => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies('Spiderman'); // Appelle la recherche au chargement
+    }, []);
+
+    return (
+        <h1>Hello APP</h1>
+    );
+};
+
+export default App;
